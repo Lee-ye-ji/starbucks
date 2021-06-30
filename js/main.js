c3ef3e93b87f77d42ff558f9a6ca4c826cca58eb
Lee-Ye-Ji <dpwl30559@gmail.com>
2021-07-01 01:08:44 +0900

    공통 모듈 분리

--- js/main.js
+++ js/main.js
@@ -1,21 +1,3 @@
-// 메뉴바 검색
-const searchEl = document.querySelector('.search');
-const searchInputEl = searchEl.querySelector('input');
-
-searchEl.addEventListener('click', function(){
-    searchInputEl.focus();
-});
-
-searchInputEl.addEventListener('focus', function(){
-    searchEl.classList.add('focused');
-    searchInputEl.setAttribute('placeholder', '통합검색');
-});
-
-searchInputEl.addEventListener('blur', function(){
-    searchEl.classList.remove('focused');
-    searchInputEl.setAttribute('placeholder', '');
-});
-
 // 메뉴바 slideDown 효과
 $('.item__name').bind('mouseover focus', function (e) {
     $(this).addClass('on');
@@ -166,6 +148,3 @@ spyEls.forEach(function(spyEl){
         .setClassToggle(spyEl, 'showScroll')
         .addTo(new ScrollMagic.Controller());
 });
-
-const thisYear = document.querySelector('.this-year');
-thisYear.textContent = new Date().getFullYear(); // 올해년도
