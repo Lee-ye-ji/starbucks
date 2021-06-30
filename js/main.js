d3f28055725f5f0c28e4d8681215e91cca13dce4
Lee-Ye-Ji <dpwl30559@gmail.com>
2021-06-30 17:26:37 +0900

    메인페이지 완성

--- js/main.js
+++ js/main.js
@@ -32,6 +32,9 @@ $('.item').bind('mouseleave', function () {
 // 오른쪽 광고 스크롤 시 사라지는 효과
 const badgeEl = document.querySelector('header .badges');;
 
+// 페이지 상단으로 이동
+const toTopEl = document.querySelector('#to-top');
+
 window.addEventListener('scroll', _.throttle(function(){
     if(window.scrollY > 500){
         // 배지 숨기기
@@ -40,15 +43,29 @@ window.addEventListener('scroll', _.throttle(function(){
             opacity: 0,
             display: 'none'
         });
+        // 버튼 보이기!
+        gsap.to(toTopEl, .2, {
+            x: 0
+        })
     }else{
         // 배지 보이기
         gsap.to(badgeEl, .6, {
             opacity: 1,
             display: 'block'
         });
+        // 버튼 숨기기!
+        gsap.to(toTopEl, .2, {
+            x: 100
+        })
     }
 }, 300));
 //  _.throttle(함수, 시간)
+
+toTopEl.addEventListener('click', function(){
+    gsap.to(window, .7, {
+        scrollTo: 0
+    });
+});
 
 
 // VISUAL 순차적으로 음료 나타내는 효과
@@ -85,6 +102,16 @@ new Swiper('.promotion .swiper-container', {
         nextEl: '.promotion .swiper-next'
     }
 });
+new Swiper('.awards .swiper-container',{
+    autoplay: true,
+    loop: true,
+    spaceBetween: 30,
+    slidesPerView: 5,
+    navigation : {
+        prevEl:'.awards .swiper-prev',
+        nextEl: '.awards .swiper-next'
+    }
+});
 
 // 스타벅스 프로모션 toggle
 const promotionEl = document.querySelector('.promotion');
@@ -102,3 +129,43 @@ promotionToggleBtn.addEventListener('click', function(){
         togglePromotionIcon.classList.remove('rotate');
     }
 });
+
+// 범위 랜덤 함수(소수점 2자리까지)
+function random(min, max) {
+    // `.toFixed()`를 통해 반환된 문자 데이터를,
+    // `parseFloat()`을 통해 소수점을 가지는 숫자 데이터로 변환
+    return parseFloat((Math.random() * (max - min) + min).toFixed(2))
+}
+
+function floatingObject(selector, delay, size){
+    // gsap.to(요소, 시간, 옵션);
+    gsap.to(
+        selector, // 선택자
+        random(1.5, 2.5), // 애니메이션 동작 시간
+        { // 옵션
+            y: size,
+            repeat: -1,
+            yoyo: true,
+            ease: Power1.easeInOut,
+            delay: random(0, delay)
+        }
+    );
+}
+floatingObject('.floating1', 1, 15);
+floatingObject('.floating2', .5, 15);
+floatingObject('.floating3', 1.5, 20);
+
+// 스크롤 위치 계산 애니메이션
+const spyEls =document.querySelectorAll('section.scroll-spy');
+spyEls.forEach(function(spyEl){
+    new ScrollMagic
+        .Scene({
+            triggerElement : spyEl, // 보여짐 여부를 감시할 요소를 지정
+            triggerHook : .8
+        })
+        .setClassToggle(spyEl, 'showScroll')
+        .addTo(new ScrollMagic.Controller());
+});
+
+const thisYear = document.querySelector('.this-year');
+thisYear.textContent = new Date().getFullYear(); // 올해년도
